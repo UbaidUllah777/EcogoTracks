a664e708935401ef95ce1b462f66eb2eccc962ae
Ubaid Ullah <khanxx777@gmail.com>
2023-09-25 11:12:08 +0500

    ubaid

--- eco-go-tracks/src/App.js
+++ eco-go-tracks/src/App.js
@@ -1,14 +1,25 @@
-import logo from "./logo.svg";
+// *** LIBRARIES ***
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//  **** LOCAL FILES/COMPONENTS ***
+import Users from "./user/pages/Users";
+import Places from "./places/pages/Places";
+
+//  *** STYLING ***
+// importing the "App.css"
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Sanga chal d asd kwana tenga ka shafiqa</p>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places">
+        <Places />
+      </Route>
+    </Router>
   );
 }
 
---
+++ eco-go-tracks/src/places/pages/Places.js
@@ -0,0 +1,12 @@
+// 1- *** LIBRARIES ***
+import React from "react";
+
+//  2- **** LOCAL FILES/COMPONENTS ***
+
+//  3- *** STYLING ***
+
+const Places = () => {
+  return <h2>Places Works</h2>;
+};
+
+export default Places;
