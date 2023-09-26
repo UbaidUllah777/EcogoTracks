2a0b9080546696c9d89431e29ce77bc477576699
Ubaid Ullah <khanxx777@gmail.com>
2023-09-26 18:48:59 +0500

    ubaid

--- eco-go-tracks/src/shared/components/Navigation/MainNavigation.js
+++ eco-go-tracks/src/shared/components/Navigation/MainNavigation.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 //  2- **** LOCAL FILES/COMPONENTS ***
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import Logo from "../../../../src/images/EcoGoLogo.png";
 
 //  3- *** STYLING ***
 import "./MainNavigation.css";
-
 const MainNavigation = (props) => {
   return (
     <MainHeader>
@@ -18,7 +18,10 @@ const MainNavigation = (props) => {
         <span />
       </button>
       <h1 className="main-navigation__title">
-        <Link to="/">ECO go Tracks</Link>
+        <Link to="/">
+          <img className="Logo" src={Logo} alt="EcoGo Logo" />{" "}
+          {/* Add alt prop */}
+        </Link>
       </h1>
       <nav className="main-navigation__header-nav">
         <NavLinks />
