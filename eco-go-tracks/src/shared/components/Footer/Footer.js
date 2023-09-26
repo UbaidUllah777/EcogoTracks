3070ce0d00f6a711271bbf3d89dba3556077c113
Ubaid Ullah <khanxx777@gmail.com>
2023-09-26 14:25:42 +0500

    ubaid

--- eco-go-tracks/src/shared/components/Footer/Footer.js
+++ eco-go-tracks/src/shared/components/Footer/Footer.js
@@ -1,16 +1,22 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import Button from "../UIElements/Button";
 
 import "./Footer.css";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear(); // Get the current year
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="container-fluid FullFooter">
       <div className="row center footerLinksRow">
         <div className="col-md-4">
-          <h3 className="footerMenuHeading">Usefull Links</h3>
+          <h3 className="footerMenuHeading">Useful Links</h3>
           <ul className="footerList">
             <li>Home</li>
             <li>About Us</li>
@@ -21,21 +27,23 @@ const Footer = () => {
         <div className="col-md-4">
           <h3 className="footerMenuHeading">Calculate Your Footprints</h3>
           <p style={{ fontSize: "0.8rem", paddingBottom: "10px" }}>
-            Incididunt sit laborum mollit velit consectetur dolore.Irure et
+            Incididunt sit laborum mollit velit consectetur dolore. Irure et
             ipsum culpa exercitation exercitation aute.
-          </p>{" "}
-          <span>
-            {" "}
-            <Button
-              to="/about-us"
-              className="primary-button "
-              buttonText="Calculate Now"
-              style={{ marginBottom: "30px" }}
-            />
-          </span>
+          </p>
+          <Button
+            to="/about-us"
+            className="primary-button"
+            buttonText="Calculate Now"
+            style={{ marginBottom: "30px" }}
+          />
         </div>
         <div className="col-md-4">
           <h3 className="footerMenuHeading">We Are Social</h3>
+          <div className="social-icons">
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
         </div>
       </div>
       <div className="row copyRightRow">
