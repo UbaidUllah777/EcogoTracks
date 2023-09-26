dc3200783e4ba56ce6c59ffa07b0999e08baf031
Muhammad Shafiq <shafiqpesh123@gmail.com>
2023-09-26 12:24:50 +0500

    shafiq

--- eco-go-tracks/src/home/components/FeaturedBlogs.js
+++ eco-go-tracks/src/home/components/FeaturedBlogs.js
@@ -5,10 +5,32 @@ import "./FeaturedBlogs.css";
 const FeaturedBlogs = () => {
   return (
     <Fragment>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8">Column 1</div>
-          <div className="col-md-4">Column 2</div>
+      <div className="container-fluid featuredblog">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <h2 className="sub-heading">Featured Blogs</h2>
+              <h1 className="main-heading">
+                {" "}
+                The Power of Green Transportation
+              </h1>
+              <p>
+                This year, Earth Overshoot Day falls on August 2nd. That’s the
+                consequence of humanity demanding 70% more than Earth’s
+                ecosystems are able to regenerate. Food makes up a large part of
+                our Ecological Footprint – feeding humanity currently takes just
+                about 50% of Earth’s biocapacity. Food is essential. With a
+                growing human population, and increasing...
+              </p>
+            </div>
+            <div className="col-md-4">
+              <img
+                className="blog-image"
+                src="home-blog-image.jpg"
+                alt="blog-image"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
---
+++ eco-go-tracks/src/home/components/GlobalFacts.js
@@ -0,0 +1,8 @@
+import React, { Fragment } from "react";
+
+import "./GlobalFacts.css";
+
+const GlobalFacts = () => {
+  return <Fragment></Fragment>;
+};
+export default GlobalFacts;
