e7f5d9cac001006fdb6b98119354066a1416b736
Ubaid Ullah <khanxx777@gmail.com>
2023-09-26 11:46:34 +0500

    u

--- eco-go-tracks/src/home/components/WhoWeAre.js
+++ eco-go-tracks/src/home/components/WhoWeAre.js
@@ -1,15 +1,43 @@
 import React from "react";
 
+import "./WhoWeAre.css";
+
 const WhoWeAre = () => {
-  return;
-  <React.Fragment>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">Column 1</div>
-        <div className="col-md-6">Column 2</div>
+  return (
+    <React.Fragment>
+      <div className="container ">
+        <div className="row ">
+          <div className="col-12 center">
+            <h1 className="globalHeading1">Measure what you treasure</h1>
+            <p className="">
+              Humans use as much ecological resources as if we lived on 1.75
+              Earths <span className="secondary-color">Source</span>. The
+              Ecological Footprint© is the only metric that compares the
+              resource demand of individuals, governments, and businesses
+              against Earth's capacity for biological regeneration.
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </React.Fragment>;
+
+      <div className="container ">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="globalHeading2">What is your impact?</h1>
+            <p>
+              Calculate your EcologicalFootprint© and personalOvershoot Day.
+            </p>
+          </div>
+          <div className="col-md-6">
+            <h1 className="globalHeading2">Passionate about data?</h1>
+            <p>
+              Check out our Ecological Footprint© Explorer open data platform.
+            </p>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default WhoWeAre;
